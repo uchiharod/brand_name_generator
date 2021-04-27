2896ea7697a481cedb43f2fb62491b78bc926a96
Roderick Moore <demetri413@gmail.com>
2021-04-27 14:49:51 -0400

    Initial Commit

---
+++ brand_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand Name Generator.")
+home_city = input("What\'s the name of the city you were born in? \n")
+pet_name = input("What\'s your pet\'s name? \n")
+
+print("Your band name could be " + home_city + " " + pet_name)
