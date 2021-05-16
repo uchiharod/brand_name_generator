330ee3bed9669acc64918933e2819fe1926a9a73
Roderick Moore <demetri413@gmail.com>
2021-05-15 22:11:47 -0400

    Adding more

--- brand_name_generator.py
+++ brand_name_generator.py
@@ -4,3 +4,4 @@ home_city = input("What\'s the name of the city you were born in? \n")
 pet_name = input("What\'s your pet\'s name? \n")
 
 print("Your band name could be " + home_city + " " + pet_name)
+#Let's try this again
