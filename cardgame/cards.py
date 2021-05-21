b5e92268a0a86802d8a31d764e8b6cca64060052
Roderick Moore <demetri413@gmail.com>
2021-05-21 15:57:41 -0400

    Let's get more code in here to be more efficient

--- cardgame/cards.py
+++ cardgame/cards.py
@@ -8,7 +8,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 playing = True
 
-#Create the Card classes
+#Create the Card classes from scratch
 class Card:
 
     def __init__(self,suit,rank):
