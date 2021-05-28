1eb9cf419bb74a323cd202763e562e04fcfb2f66
Roderick Moore <demetri413@gmail.com>
2021-05-27 23:48:59 -0400

    Adding comment on the python files

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -1,4 +1,4 @@
-# Make a dictionary with {} and : to signify a key and a value
+# Make a dictionary with {} and : to signify a key and a value adding more later
 my_dict = {'key1':'value1','key2':'value2'}
 
 my_dict['key2']
