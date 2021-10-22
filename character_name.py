2c2504b6582723d4be4f99277b0418006dc486c9
Roderick Moore <demetri413@gmail.com>
2021-10-22 15:44:06 -0400

    Adding new python project to the mix

---
+++ character_name.py
@@ -0,0 +1,13 @@
+character_name = "John"
+character_age = 50
+print("There once was a man named " + character_name + ", ")
+print("he was " + str(character_age) + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + str(character_age) + ". ")
+print("----------------------------------------")
+character_name = "Mike"
+character_age = 38
+print("There once was a man named " + character_name + ", ")
+print("he was " + str(character_age) + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + str(character_age) + ". ")
