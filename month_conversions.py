70568384e60cfbed82c85dd874f6f7a6445983a7
Roderick Moore <demetri413@gmail.com>
2021-11-27 21:16:25 -0500

    Adding the month converter

---
+++ month_conversions.py
@@ -0,0 +1,15 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+print(monthConversions.get("Jun", "Not a valid key")) #get method lets you specify something when a key is not found
